cc35d31761f187a94aaf1e92c4e5657358f4ac9b
Dmytro Tarasenko <dima.tarasenko.52@gmail.com>
2026-08-18 15:16:18 +0300

    feat: add CLI entry point with loading spinners
    Integrated ora and chalk for a better terminal experience while the parser builds the architecture graph.

--- bin/cli.js
+++ bin/cli.js
@@ -1,7 +1,25 @@
 #!/usr/bin/env node
 
-import { findFiles } from "../src/analyser/walker.js";
-import { readTheFile } from "../src/analyser/lexer.js";
+import ora from "ora";
+import chalk from "chalk";
+import { buildGraph } from "../src/analyser/parser.js";
 
-const files = findFiles("./");
-files.forEach((file) => console.log(file, readTheFile(file)));
+console.log(chalk.bold.magenta("\nStarting Architecture Analysis...\n"));
+
+const spinner = ora("Scanning directories and tokenising files...").start();
+
+try {
+  setTimeout(() => {
+    const graphData = buildGraph("./");
+
+    spinner.succeed(
+      chalk.green(`Successfully parsed ${graphData.length} files.`),
+    );
+    console.log(chalk.gray("Ready to launch the web interface..."));
+
+    // todo: launch server and webpage
+  }, 1000);
+} catch (error) {
+  spinner.fail(chalk.red("Failed to build the architecture graph."));
+  console.error(error);
+}
